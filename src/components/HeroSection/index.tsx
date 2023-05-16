9f9a8f4fe6c26a0e534421ff79dae94d1255d3b3
pedrohmp <pedrohmp@icloud.com>
2023-05-15 22:30:18 -0300

    chore: savepoint

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -8,17 +8,17 @@ export const HeroSection = () => {
   return (
     <div
       style={{ height: '40vh' }}
-      className='flex items-center justify-between bg-black gap-96'
+      className='flex items-center justify-between bg-black gap-20'
     >
-      <div className='h-fit flex-auto pl-20'>
-        <h1 className='mb-10 text-4xl text-white w-10/12'>
+      <div className='h-fit flex-auto pl-20 max-w-full'>
+        <h1 className='mb-10 text-4xl text-white w-full max-w-sm'>
           Blogging about code and life.
         </h1>
 
         <input
           type='text'
           id='last_name'
-          className='block w-full rounded-lg border border-gray-300 bg-transparent p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
+          className='w-full max-w-md rounded-lg border border-gray-300 bg-transparent p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
           placeholder='Search a post here'
         />
 
